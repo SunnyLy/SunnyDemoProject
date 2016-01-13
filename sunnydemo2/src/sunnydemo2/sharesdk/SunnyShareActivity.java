16ff9ce53b9c3a3fda61fdae972087d5fd72d0c0
SunnyLy <450512747@qq.com>
2016-01-13 19:59:11 +0800

    分享Model分离。。。。。。。。。。。。

--- sunnydemo2/src/sunnydemo2/sharesdk/SunnyShareActivity.java
+++ sunnydemo2/src/sunnydemo2/sharesdk/SunnyShareActivity.java
@@ -11,7 +11,9 @@ import com.het.share.dialog.CommonShareDialog;
 import com.het.share.listener.ICommonShareLinstener;
 import com.het.share.manager.CommonShareManager;
 import com.het.share.manager.CommonSharePlatform;
-import com.het.share.model.CommonShareBean;
+import com.het.share.model.CommonShareBaseBean;
+import com.het.share.model.CommonShareMusic;
+import com.het.share.model.CommonShareWebpage;
 import com.smartbracelet.sunny.sunnydemo2.R;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.UiError;
@@ -29,8 +31,8 @@ public class SunnyShareActivity extends Activity implements ICommonShareLinstene
     private String musicUrl = "http://staff2.ustc.edu.cn/~wdw/softdown/index.asp/0042515_05.ANDY.mp3";
     private String musicDataUrl = "http://staff2.ustc.edu.cn/~wdw/softdown/index.asp/0042515_05.ANDY.mp3";
     private String mTargetUrl = "http://user.qzone.qq.com/450512747/infocenter?ptsig=vpvnl-fJW5GE6ROJxYC3-U4Ah0KsYVZD0m3jkC3Q-Yo_";
-    private String mTitle = "我是艳国，我是Sunny";
-    private String mDescription = "我为自己代言，想听就听吧！";
+    private String mTitle = "阳总";
+    private String mDescription = "Hi,阳总,吃晚饭了没";
     private String mImgUrl = "http://pic15.nipic.com/20110725/7067632_020203324179_2.jpg";
 
     /*private String mWeixinAppId = "wxd930ea5d5a258f4f";//微信Demo用
@@ -76,23 +78,48 @@ public class SunnyShareActivity extends Activity implements ICommonShareLinstene
     @Override
     public void onStartShare(CommonSharePlatform sharePlatform) {
 
-        CommonShareBean shareBean = new CommonShareBean();
+        CommonShareBaseBean shareBean = new CommonShareBaseBean();
         shareBean.setUiListener(this);
         shareBean.setTitle(mTitle);
         shareBean.setDescription(mDescription);
         shareBean.setAppName("SunnyDemo");
         shareBean.setTargetUrl(mTargetUrl);
-        shareBean.setMusicUrl(musicUrl);
-        shareBean.setMusicDataUrl(musicDataUrl);
         shareBean.setBm(null);
         shareBean.setSharePlatform(sharePlatform);
 
+
+        /**
+         * 音乐
+         */
+        CommonShareMusic music = new CommonShareMusic();
+        music.setUiListener(this);
+        music.setTitle(mTitle);
+        music.setDescription(mDescription);
+        music.setAppName("SunnyDemo");
+        music.setTargetUrl(mTargetUrl);
+        music.setMusicUrl(musicUrl);
+        music.setMusicDataUrl(musicDataUrl);
+        music.setBm(null);
+        music.setSharePlatform(sharePlatform);
+
+
+        /**
+         * 网页
+         */
+        CommonShareWebpage webpage = new CommonShareWebpage();
+        webpage.setUiListener(this);
+        webpage.setTitle(mTitle);
+        webpage.setDescription(mDescription);
+        webpage.setAppName("SunnyDemo");
+        webpage.setTargetUrl(mTargetUrl);
+        webpage.setBm(null);
+        webpage.setSharePlatform(sharePlatform);
+
         //分享音乐
-        mShareManger.shareMusic(shareBean);
+        mShareManger.shareMusic(music);
         //分享网页
-        /*shareBean.setImgUrl(mImgUrl);
-        mShareManger.shareWebpage(shareBean);*/
-        Toast.makeText(this,"点击："+sharePlatform,Toast.LENGTH_SHORT).show();
+       /* webpage.setImgUrl(mImgUrl);
+        mShareManger.shareWebpage(webpage);*/
     }
 
     @Override
