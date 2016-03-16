c9f80e0086095d34525556f68be43a3d8e600ffd
SunnyLy <450512747@qq.com>
2016-03-16 11:14:30 +0800

    Twttiter的Fabric错误日志分析。。。。。。

--- sunnydemo3/src/main/java/com/smartbracelet/sunny/sunnydemo3/MainActivity.java
+++ sunnydemo3/src/main/java/com/smartbracelet/sunny/sunnydemo3/MainActivity.java
@@ -5,15 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            int i = 1/0;
+            Log.e("errorMsg",i+"");
             SettingsActivity.startSettingsActivity(MainActivity.this);
             return true;
         }
