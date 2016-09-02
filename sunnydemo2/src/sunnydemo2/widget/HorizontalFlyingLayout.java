02158c4981798eef386af18fdf6c96c3eea68da8
SunnyLy <450512747@qq.com>
2016-09-02 13:18:44 +0800

    'HorizontalFlyingLayout'

---
+++ sunnydemo2/src/sunnydemo2/widget/HorizontalFlyingLayout.java
@@ -0,0 +1,15 @@
+package sunnydemo2.widget;
+
+/**
+ * ------------------------------------------------
+ * Copyright © 2016年 clife. All rights reserved.
+ * Shenzhen H&T Intelligent Control Co.,Ltd.
+ * -----------------------------------------------
+ *
+ * @Author sunny
+ * @Date 2016/9/2  10:26
+ * @Version v1.0.0
+ * @Annotation
+ */
+public class HorizontalFlyingLayout {
+}
